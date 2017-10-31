5f59f2b8abc587ce12022cbe3ce1a42891bc185a
Igor <grammatchikov@inbox.ru>
2017-10-31 17:44:38 +0300

    Добавление элементов в корзину при клике

--- public/scripts/phones-page.js
+++ public/scripts/phones-page.js
@@ -4,8 +4,14 @@
 class PhonesPage{
     constructor(options){
         this._el = options.el;
+        this._cart = new ShoppingCart({
+            el: this._el.querySelector('[data-component="shopping-cart"]')
+        });
         this._catalogue = new PhoneCatalogue({
             el: this._el.querySelector('[data-component="phone-catalogue"]')
+        });
+        this._catalogue._el.addEventListener('phoneSelected', (event) => {
+            this._cart.addItem({ id: event.detail });
         })
     }
 }
